4828b5d3f20ee9f1aa956d4139510a93661779f2
Allgot <96973603+Allgot@users.noreply.github.com>
2022-10-01 12:38:50 +0900

    v0.8.15: fixed a bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-sudo su <<EOF
+# sudo su <<EOF
 
-opam install dune # merlin ocaml-lsp-server odoc ocamlformat utop dune-release --yes
+# opam install dune --yes
+# eval $(opam config env)
+
+# dune build OcamlTest.exe
+
+# dune exec ./hello_world.exe
+# EOF
+
+opam install dune --yes
 eval $(opam config env)
 
 dune build OcamlTest.exe
 
 dune exec ./hello_world.exe
-EOF
 
 echo "Hello $1"
 time=$(date)
