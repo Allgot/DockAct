ff903f3f0e5f21950b64a5d40212a06c93a3d1f3
Allgot <dlwlstj496@gmail.com>
2022-10-05 22:45:27 +0900

    v0.9.6: fixed a bug

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM prosyslab/classroom
 COPY entrypoint.sh /entrypoint.sh
-USER 0
+USER 0\
+RUN opam init --yes --disable-sandboxing
+RUN eval $(opam env --switch=default)
+RUN opam install dune
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-echo "1234" | su -
-
-opam init --yes --disable-sandboxing
-eval $(opam env --switch=default)
-
 dune build ocaml_test.exe
 
 dune exec ./ocaml_test.exe
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+# echo "Hello $1"
+# time=$(date)
+# echo "::set-output name=time::$time"
