2a85c9c0d8f2d74c83fb9ef3c15cbdedb0ec4c37
Allgot <96973603+Allgot@users.noreply.github.com>
2022-10-06 15:18:59 +0900

    v1.0.5: debugging

--- ocaml_test.ml
+++ ocaml_test.ml
@@ -1,2 +1,4 @@
-let _ = print_endline "executed!" in
-    print_endline "::set-output name=string::Test Ocaml"
+(* let issue_num = Sys.argv.(1) in
+    let issue_contents = Sys.argv.(2) in
+        let _ = (Printf.printf "%i and %s" issue_num issue_contents) in *)
+            print_endline "::set-output name=string::Test Ocaml"
