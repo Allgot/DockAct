af7950ef4b6f2abdf6fd37ae77d088f0ff949846
Allgot <96973603+Allgot@users.noreply.github.com>
2022-11-27 19:59:48 +0900

    In debugging.

--- ocaml_test.ml
+++ ocaml_test.ml
@@ -29,14 +29,12 @@ let rec get_issues page_num res =
 
     if List.length issue_list == 0 then []
     else
-        let _ = Printf.printf "AAAAAAAAAAAAAAAAAAAAAAAAAAAAa" in
         let _ = List.iter2 (fun content num ->
             Printf.printf "contnet: %s, num: %d\n" content num;
             if num != int_of_string (Sys.argv.(1)) then
             let _ = map_ConNum := ConNum.add content num !map_ConNum in
             Printf.printf "put %d with the key of %s\n" num content;
         ) issue_list num_list in
-        let _ = Printf.printf "BBBBBBBBBBBBBBBBBBBBBBBBBBB" in
                 
         issue_list @ (get_issues (page_num+1) res)
 
@@ -60,26 +58,32 @@ let () = List.iter (fun issue_contents ->
                 let cur_sim = List.hd ([json_body] |> filter_member "similarity" |> filter_number) in
                     if cur_sim > threshold_sim && cur_sim > !max_sim then
                         let _ = max_sim := cur_sim in
-                        max_contents := issue_contents
-) (get_issues 0 [])
+                        let _ = max_contents := issue_contents in
+                        Printf.printf "max updated! sim %f, contents %s\n" !max_sim !max_contents;
+) (get_issues 1 [])
 
 let _ =
-    if !max_sim == -1.0 then Sys.command ("echo \"dup_num=" ^ (Int.to_string (-1)) ^ "\" >> $GITHUB_OUTPUT")
+    if !max_sim == -1.0 then Sys.command ("echo \"dup_num=-1\" >> $GITHUB_OUTPUT")
     else Sys.command ("echo \"dup_num=" ^ (Int.to_string (ConNum.find !max_contents !map_ConNum)) ^ "\" >> $GITHUB_OUTPUT")
 
-let _ =
-    if !max_sim == -1.0 then ()
-    else 
-        (* Leave a comment *)
-        let post_body = Cohttp_lwt.Body.of_string (Yojson.Basic.to_string (
-            `Assoc[("body", (`String ("Possible duplication detected. Refer to #" ^ (Int.to_string (ConNum.find !max_contents !map_ConNum)))))]
-        )) in
-        let post_header = Cohttp.Header.add_authorization (Cohttp.Header.init_with "accept" "application/vnd.github+json") (Cohttp.Auth.credential_of_string ("Bearer " ^ Sys.argv.(5))) in
-        let body =
-            Client.post ~body:post_body ~headers:post_header (Uri.of_string ("https://api.github.com/repos/" ^ Sys.argv.(3) ^ "/issues/" ^ Sys.argv.(1) ^ "/comments")) >>= fun (resp, body) ->
-                let code = resp |> Response.status |> Code.code_of_status in
-                    Printf.printf "Response code: %d\n" code;
-                Cohttp_lwt.Body.to_string body in
+let detected_num = (ConNum.find !max_contents !map_ConNum)
 
-        let body = Lwt_main.run body in
-                print_endline body
+let body =
+    (* Leave a comment *)
+    let comment_body = Cohttp_lwt.Body.of_string (Yojson.Basic.to_string (
+        `Assoc[("body", (`String ("Possible duplication detected. Refer to #" ^ (Int.to_string (ConNum.find !max_contents !map_ConNum)))))]
+    )) in
+    let comment_header = Cohttp.Header.add_authorization (Cohttp.Header.init_with "accept" "application/vnd.github+json") (Cohttp.Auth.credential_of_string ("Bearer " ^ Sys.argv.(5))) in
+
+    Client.post 
+        ~body:comment_body 
+        ~headers:comment_header 
+        (Uri.of_string ("https://api.github.com/repos/" ^ Sys.argv.(3) ^ "/issues/" ^ Sys.argv.(1) ^ "/comments")) 
+        >>= fun (resp, body) ->
+            let code = resp |> Response.status |> Code.code_of_status in
+                Printf.printf "Response code: %d\n" code;
+            Cohttp_lwt.Body.to_string body
+
+let () = 
+    let body = Lwt_main.run body in
+    print_endline body
