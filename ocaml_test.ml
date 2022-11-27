7f03589ee7791212fee68633b06969962f8c8312
Allgot <96973603+Allgot@users.noreply.github.com>
2022-11-27 18:50:30 +0900

    Updated to dude-test again.

--- ocaml_test.ml
+++ ocaml_test.ml
@@ -1,4 +1,5 @@
 open Lwt
+open Cohttp
 open Cohttp_lwt_unix
 
 module ConNum = Map.Make(String)
@@ -26,8 +27,12 @@ let rec get_issues page_num res =
     let issue_list = fst issue_tup in
     let num_list = snd issue_tup in
 
-    let _ = List.iter2 (fun content num -> 
-        map_ConNum := ConNum.add content num !map_ConNum) issue_list num_list in 
+    let _ = List.iter2 (fun content num ->
+        if num != int_of_string (Sys.argv.(1)) then
+          let _ = map_ConNum := ConNum.add content num !map_ConNum in
+          Printf.printf "put %d with the key of %s\n" num content;
+    ) issue_list num_list in
+            
 
     if List.length issue_list == 0 then []
     else issue_list @ (get_issues (page_num+1) res)
@@ -35,17 +40,16 @@ let rec get_issues page_num res =
 let sim_header = Cohttp.Header.of_list [("X-RapidAPI-Key", Sys.argv.(4)); ("X-RapidAPI-Host", "twinword-text-similarity-v1.p.rapidapi.com"); ("content-type", "application/x-www-form-urlencoded")]
 
 let threshold_sim = 0.20
-let max_sim = ref 0.0
+let max_sim = ref (-1.0)
 let max_contents = ref ""
 
 let () = List.iter (fun issue_contents -> 
     let text1 = Yojson.Basic.to_string (`String Sys.argv.(2)) in
     let text2 = Yojson.Basic.to_string (`String issue_contents) in
-    if not (String.equal text1 text2) then
-        let () = Printf.printf "Compare %s with %s\n" text1 text2 in
-            let body =
-                Client.get  ~headers:sim_header (Uri.of_string ("https://twinword-text-similarity-v1.p.rapidapi.com/similarity/?" ^ "text1=" ^ text1 ^ "&" ^ "text2=" ^ text2)) >>= fun (_, body) ->
-                    Cohttp_lwt.Body.to_string body in
+    let () = Printf.printf "Compare %s with %s\n" text1 text2 in
+        let body =
+            Client.get  ~headers:sim_header (Uri.of_string ("https://twinword-text-similarity-v1.p.rapidapi.com/similarity/?" ^ "text1=" ^ text1 ^ "&" ^ "text2=" ^ text2)) >>= fun (_, body) ->
+                Cohttp_lwt.Body.to_string body in
 
     let body = Lwt_main.run body in
         let json_body = Yojson.Basic.from_string body in
@@ -56,4 +60,23 @@ let () = List.iter (fun issue_contents ->
                         max_contents := issue_contents
 ) (get_issues 0 [])
 
-let _ = Sys.command ("echo \"dup_num=" ^ (Int.to_string (ConNum.find !max_contents !map_ConNum)) ^ "\" >> $GITHUB_OUTPUT")
+let _ =
+    if !max_sim == -1.0 then Sys.command ("echo \"dup_num=" ^ (Int.to_string (-1)) ^ "\" >> $GITHUB_OUTPUT")
+    else Sys.command ("echo \"dup_num=" ^ (Int.to_string (ConNum.find !max_contents !map_ConNum)) ^ "\" >> $GITHUB_OUTPUT")
+
+let _ =
+    if !max_sim == -1.0 then ()
+    else 
+        (* Leave a comment *)
+        let post_body = Cohttp_lwt.Body.of_string (Yojson.Basic.to_string (
+            `Assoc[("body", (`String ("Possible duplication detected. Refer to #" ^ (Int.to_string (ConNum.find !max_contents !map_ConNum)))))]
+        )) in
+        let post_header = Cohttp.Header.add_authorization (Cohttp.Header.init_with "accept" "application/vnd.github+json") (Cohttp.Auth.credential_of_string ("Bearer " ^ Sys.argv.(5))) in
+        let body =
+            Client.post ~body:post_body ~headers:post_header (Uri.of_string ("https://api.github.com/repos/" ^ Sys.argv.(3) ^ "/issues/" ^ Sys.argv.(1) ^ "/comments")) >>= fun (resp, body) ->
+                let code = resp |> Response.status |> Code.code_of_status in
+                    Printf.printf "Response code: %d\n" code;
+                Cohttp_lwt.Body.to_string body in
+
+        let body = Lwt_main.run body in
+                print_endline body
