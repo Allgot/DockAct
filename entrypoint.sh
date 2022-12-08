31e49b72694092b4f3f669aa8f5940db867bede2
Allgot <dlwlstj496@gmail.com>
2022-12-08 11:36:30 +0900

    dbgg.

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,4 +18,4 @@ pwd
 echo "-------------------ls-----------------------"
 ls
 echo "------------------dune------------------------"
-dune exec /github/workspace/dup_scan.exe $1 "contents:$2" $3 $4 "$5" "$6" # $1: issue_num, $2: issue_contents, $3: repository_path_name, $4: rapid_key, $5: repo_key, $6: threshold
+dune exec -- ./dup_scan.exe $1 "contents:$2" $3 $4 "$5" "$6" # $1: issue_num, $2: issue_contents, $3: repository_path_name, $4: rapid_key, $5: repo_key, $6: threshold
